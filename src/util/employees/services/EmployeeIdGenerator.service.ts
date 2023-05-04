ff67d71bb4b93ac35b324469b2ad09bab364da5e
Jian Hong <77224948+jianhong1998@users.noreply.github.com>
2023-05-04 13:26:46 +0800

    Update on the way of generating new employeeId

--- src/util/employees/services/EmployeeIdGenerator.service.ts
+++ src/util/employees/services/EmployeeIdGenerator.service.ts
@@ -1,7 +1,11 @@
 import EmployeeStorageController from "../controllers/EmployeeStorage.controller";
 
 export default class EmployeeIdGenerator {
+    private static idRunningNumber = EmployeeStorageController.size();
+    
     public static generateEmployeeId(): number {
-        return EmployeeStorageController.size() + 1;
+        this.idRunningNumber++;
+        
+        return this.idRunningNumber;
     }
 }
